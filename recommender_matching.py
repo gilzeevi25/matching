311c99ad1053ae48a765eee01eb8c36bd71283d9
Gil Zeevi <gilsmol91@gmail.com>
2022-08-04 11:47:34 +0300

    Update recommender_matching.py

--- recommender_matching.py
+++ recommender_matching.py
@@ -207,7 +207,7 @@ st.dataframe(data=df, width=None, height=None)
 
 @st.cache
 def convert_df(df):
-   return df.to_csv().encode('utf-8')
+   return df.to_csv().encode('utf-8-sig')
 
 
 csv = convert_df(df)
